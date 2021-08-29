064e283c4c7cc7d97c9bf8f32b6b47e9845e4134
ANKUR SHARMA <ankur02srma@gmail.com>
2021-08-30 00:39:51 +0530

    todo with redux

--- react_redux_todoApp/modera_redux_todo/src/App.js
+++ react_redux_todoApp/modera_redux_todo/src/App.js
@@ -1,16 +1,44 @@
-import 'bulma/css/bulma.css'
-
+import "bulma/css/bulma.css";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo, deleteTodo, removeTodo } from "./action/index";
 import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch();
+
+  const list = useSelector((state) => state.todoReducer.list);
+
+  const handleClick = () => {
+    dispatch(addTodo(input));
+    setInput("");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Todo app with the use of modern Redux ✍</h1>
-        <div>
-          <input class="input is-primary" type="text" placeholder="Add todo" />
-          <button class="button is-success">+</button>
+        <div style={{ display: "flex", margin: "10px" }}>
+          <input
+            className="input is-primary"
+            type="text"
+            placeholder="Add todo"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button className="button is-success" onClick={handleClick}>
+            +
+          </button>
         </div>
+
+        {list.map((data) => {
+          return (
+          <div style={{ display: "flex", margin: "5px" }} key={data.id}>
+            <p style={{ width: "30rem", textAlign: "left" }}> {data.data} </p>
+            <button className="button is-danger" onClick={() => dispatch(deleteTodo(data.id))} >X</button>
+          </div>)
+        })}
       </header>
     </div>
   );
---
+++ react_redux_todoApp/modera_redux_todo/src/reducer/todoReducers.js
@@ -0,0 +1,32 @@
+const initialData = {
+  list: [],
+};
+// same as const [list, setList] = useState([])
+
+const todoReducer = (state = initialData, action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      const { id, data } = action.payload;
+      return {
+        ...state,
+        list: [
+          ...state.list,
+          {
+            id: id,
+            data: data,
+          },
+        ],
+      };
+
+    case "DELETE_TODO": 
+      const newList = state.list.filter((elem) => elem.id !== action.id)
+      return {
+        ...state,
+        list: newList
+      }
+
+    default: return state;
+  }
+};
+
+export default todoReducer;
